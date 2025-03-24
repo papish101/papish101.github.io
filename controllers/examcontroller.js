ffe6c59b8c950ef2c8d95b8dccc246c819997863
papish101 <murphyobinyan@yahoo.com>
2025-03-24 10:56:25 +0100

    Update examcontroller.js

--- controllers/examcontroller.js
+++ controllers/examcontroller.js
@@ -1,1 +1,23 @@
 
+const Exam = require('../models/Exam');
+
+exports.createExam = async (req, res) => {
+  try {
+    const exam = new Exam(req.body);
+    await exam.save();
+    res.status(201).json(exam);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getExams = async (req, res) => {
+  try {
+    const exams = await Exam.find();
+    res.status(200).json(exams);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Add more exam-related functions as needed
