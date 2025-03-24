bfc83de01806de9381a7db605070381e2de61d37
papish101 <murphyobinyan@yahoo.com>
2025-03-24 12:46:14 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/exams', examRoutes);
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/cbt_exam', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:5000/cbt_exam', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
