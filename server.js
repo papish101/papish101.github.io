83299b34a4ec2f8171cd5970fc72eab3086cc92d
papish101 <murphyobinyan@yahoo.com>
2025-03-24 15:35:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const examRoutes = require('./routes/examRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
