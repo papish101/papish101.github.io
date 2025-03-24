1fe08615278368326b0336bf640de2f8c2db0d11
papish101 <murphyobinyan@yahoo.com>
2025-03-24 17:30:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors({
-    origin: 'https://papish101.github.io/#form-title',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-}));
-
 const userRoutes = require('./routes/userRoutes');
 const examRoutes = require('./routes/examRoutes');
 
@@ -16,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://papish101.github.io', // Update to your frontend base URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
 
 // Routes
 app.use('/api/users', userRoutes);
