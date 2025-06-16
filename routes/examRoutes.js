e34414135b8a906dbfbc03f231c7c8c358ce5791
papish101 <murphyobinyan@yahoo.com>
2025-06-16 04:38:57 +0100

    Delete routes directory

--- routes/examRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const examController = require('../controllers/examController');
-
-const router = express.Router();
-
-router.post('/', examController.createExam);
-router.get('/', examController.getExams);
-
-module.exports = router;
